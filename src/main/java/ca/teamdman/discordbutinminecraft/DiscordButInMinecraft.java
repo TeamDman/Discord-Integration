f5f3c0f04e87635dc271b7eb881c82d39539bd0b
TeamDman <TeamDman9201@gmail.com>
2019-03-08 01:46:00 -0500

    :circus_tent: Basic Workspace

---
+++ src/main/java/ca/teamdman/discordbutinminecraft/DiscordButInMinecraft.java
@@ -0,0 +1,9 @@
+package ca.teamdman.discordbutinminecraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=DiscordButInMinecraft.MODID, name = "Discord, But In Minecraft", version = "@VERSION")
+public class DiscordButInMinecraft {
+	static final String MODID = "discordbutinminecraft";
+
+}
